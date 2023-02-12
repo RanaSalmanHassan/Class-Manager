46da8c826760e302e50e38e71e7399b69a7c4e81
Salman <ranasalmanhassan2007@gmail.com>
2023-02-12 11:59:00 +0500

    Added Inbox and View Assignments and View Teachers List for students

--- portfolio/Student_App/urls.py
+++ portfolio/Student_App/urls.py
@@ -5,6 +5,8 @@ app_name = 'student_app'
 urlpatterns = [
     path('', views.student_home, name='student_home'),
     path('message_to_teacher', views.message_to_teacher, name='message_to_teacher'),
-    path('messages_all', views.messages_all, name='messages_all'),
+    path('inbox', views.inbox, name='inbox'),
     path('student_options', views.student_options, name='student_options'),
+    path('all_assignments', views.all_assignments, name='all_assignments'),
+    path('all_teachers', views.all_teachers, name='all_teachers'),
 ]
--- portfolio/Student_App/views.py
+++ portfolio/Student_App/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
-from LOGIN_APP.models import Message
+from LOGIN_APP.models import Message,TeacherProfile
 from LOGIN_APP.forms import MessageForm
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.urls import reverse
+from Teacher_App.models import Assignment
+
 
 # Create your views here.
 
@@ -30,14 +32,26 @@ def message_to_teacher(request):
 
 
 @login_required(login_url='loginapp:Login')
-def messages_all(request):
+def inbox(request):
     sent_messages = Message.objects.filter(user_sender=request.user)
     recieved_messages = Message.objects.filter(user_reciever=request.user)
     dict = {'sent_messages': sent_messages,
             'recieved_messages': recieved_messages}
-    return render(request, 'student_app/messages_all.html', dict)
+    return render(request, 'student_app/inbox.html', dict)
 
 
 @login_required(login_url='loginapp:Login')
 def student_options(request):
     return render(request, 'student_app/student_options.html')
+
+@login_required(login_url='loginapp:Login')
+def all_assignments(request):
+    all_assignments_for_students = Assignment.objects.all()
+    dict = {'all_assignments_for_students':all_assignments_for_students}
+    return render(request,'student_app/all_assignments.html',dict)
+
+@login_required(login_url='loginapp:Login')
+def all_teachers(request):
+    all_teachers_of_school = TeacherProfile.objects.all()
+    dict = {'all_teachers_of_school':all_teachers_of_school}
+    return render(request,'student_app/all_teachers.html',dict)
